7d9910f4255542d74bd5ab4721360cb0788200a2
sagarika <sagarikamandhan@gmail.com>
2017-12-02 12:09:53 +0530

    setting up dev environment

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var app = express();
+const PORT = 3000;
+
+//database connection
+mongoose.connect('mongodb://localhost:27017/shopping/backend');
+
+//check database connection
+mongoose.connection.on('connected', function(){
+  console.log("connected to database");
+});
+mongoose.connection.on('error',function(err){
+  console.log(err);
+});
+
+//testing server
+app.listen(PORT, function(){
+  console.log('server is running at port '+ PORT);
+});
